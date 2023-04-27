a4e5da5ddac70b0d24513ccc814b6274175ce9c4
Владислав Шиляев <schilyaev.vlad@gmail.com>
2023-04-27 21:59:18 +0300

    Add publishing config

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,8 +1,31 @@
 plugins {
     alias(libs.plugins.kotlin.jvm) apply false
+    `java-library`
+    `maven-publish`
 }
 
 allprojects {
     version = "0.1.0-SNAPSHOT"
     group = "com.ithersta"
+}
+
+subprojects {
+    apply(plugin = "java-library")
+    apply(plugin = "maven-publish")
+
+    java {
+        withSourcesJar()
+        withJavadocJar()
+    }
+
+    publishing {
+        publications {
+            create<MavenPublication>("maven") {
+                groupId = group.toString()
+                artifactId = project.name
+                version = project.version.toString()
+                from(components["java"])
+            }
+        }
+    }
 }
