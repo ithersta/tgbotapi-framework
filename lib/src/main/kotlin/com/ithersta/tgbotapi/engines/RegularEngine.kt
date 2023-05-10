f63f9a1f86a01b742d2eb02be266703cb83ee92e
Владислав Шиляев <schilyaev.vlad@gmail.com>
2023-05-10 17:20:48 +0300

    Never catch CancellationException

--- lib/src/main/kotlin/com/ithersta/tgbotapi/engines/RegularEngine.kt
+++ lib/src/main/kotlin/com/ithersta/tgbotapi/engines/RegularEngine.kt
@@ -1,12 +1,13 @@
 package com.ithersta.tgbotapi.engines
 
-import com.ithersta.tgbotapi.core.runner.StatefulRunner
 import com.ithersta.tgbotapi.core.Dispatcher
+import com.ithersta.tgbotapi.core.runner.StatefulRunner
 import com.ithersta.tgbotapi.core.runner.statefulRunner
 import dev.inmo.micro_utils.coroutines.subscribeSafelyWithoutExceptionsAsync
 import dev.inmo.tgbotapi.bot.TelegramBot
 import dev.inmo.tgbotapi.extensions.utils.extensions.sourceChat
 import dev.inmo.tgbotapi.utils.PreviewFeature
+import kotlinx.coroutines.CancellationException
 
 @OptIn(PreviewFeature::class)
 public fun Dispatcher.regularEngine(
@@ -16,8 +17,12 @@ public fun Dispatcher.regularEngine(
     allUpdatesFlow.subscribeSafelyWithoutExceptionsAsync(scope, { it.sourceChat() }) { update ->
         runCatching {
             handle(update)
-        }.onFailure {
-            exceptionHandler(bot, it)
+        }.onFailure { exception ->
+            if (exception is CancellationException) {
+                throw exception
+            } else {
+                exceptionHandler(bot, exception)
+            }
         }
     }
 }
